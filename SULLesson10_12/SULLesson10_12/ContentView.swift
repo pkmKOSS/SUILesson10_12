eadd14f50c21d9f3f720df565fffe42da4747922
Григоренко Александр Игоревич <aigrigorenko@mtsbank.ru>
2023-01-23 19:52:13 +0500

    initCommit

---
+++ SULLesson10_12/SULLesson10_12/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SULLesson10_12
+//
+//  Created by Григоренко Александр Игоревич on 23.01.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SULLesson10_12/SULLesson10_12/SULLesson10_12App.swift
@@ -0,0 +1,17 @@
+//
+//  SULLesson10_12App.swift
+//  SULLesson10_12
+//
+//  Created by Григоренко Александр Игоревич on 23.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct SULLesson10_12App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
